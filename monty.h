bc59d9d2ff23600bdc98fbaf59280dd9dedf7a8a
asaad-chadi <125455763+asaad-chadi@users.noreply.github.com>
2023-08-24 18:59:16 +0100

    s

--- monty.h
+++ monty.h
@@ -31,7 +31,7 @@ extern int value;
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integers
  * @prev: points to the previous elements of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
+ * @next: points to the next elements of the stack (or queue)
  *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
