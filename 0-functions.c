00ecdc5981479c2aaae1253be17db2085f8a4f48
asaad-chadi <125455763+asaad-chadi@users.noreply.github.com>
2023-08-24 18:45:14 +0100

    s add

--- 0-functions.c
+++ 0-functions.c
@@ -66,7 +66,7 @@ void pall(stack_t **stack, unsigned int line_number)
 
 /**
  * pint - printing the value at the top of the stacks
- * @stack: double pointer to the head of the stack
+ * @stack: double pointers to the head of the stack
  * @line_number: the current line number
  */
 
